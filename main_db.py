9ce34d83a4ddff7f4b878c5ead803f15f0d503e9
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-02-01 20:05:21 +0200

    open new branch

--- main_db.py
+++ main_db.py
@@ -13,6 +13,17 @@ def get_table_class(table_name):
     return eval(a)
 
 
+def convert_choice(choice):
+
+    if choice == 'None':
+        return -1
+    elif choice == "left":
+        return 0
+    elif choice == "right":
+        return 1
+    else:
+        raise ValueError
+
 def main():
 
     summary_entries = Summary.objects.all()
@@ -22,59 +33,41 @@ def main():
             session = get_table_class(sm.session_table)
 
             for trial in session.objects.all():
-
-                new_trial = ExperimentalData(
-
+                fixation_time = eval(sm.fixation_time)
+                inter_trial_time = eval(sm.inter_trial_time)
+                choice = convert_choice(trial.choice)
+                new_trial = ExperimentalData.objects.create(
                     monkey=sm.monkey,
-                    date = datetime.strptime(string_time, '%Y-%m-%d %H:%M:%S.%f'),
-                fixation_time_min = models.IntegerField(blank=True, null=True)
-                fixation_time_max = models.IntegerField(blank=True, null=True)
-                initial_stock = models.IntegerField(blank=True, null=True)
-                inter_trial_time_min = models.IntegerField(blank=True,
-                                                           null=True)
-                inter_trial_time_max = models.IntegerField(blank=True,
-                                                           null=True)
-                max_decision_time = models.IntegerField(blank=True, null=True)
-                max_return_time = models.IntegerField(blank=True, null=True)
-                punishment_time = models.IntegerField(blank=True, null=True)
-                result_display_time = models.IntegerField(blank=True,
-                                                          null=True)
-                reward_time = models.IntegerField(blank=True, null=True)
-                trials_per_block = models.IntegerField(blank=True, null=True)
-                valve_opening_time = models.IntegerField(blank=True, null=True)
-                control_trials_proportion = models.IntegerField(blank=True,
-                                                                null=True)
-                incongruent_proportion = models.IntegerField(blank=True,
-                                                             null=True)
-                with_losses_proportion = models.IntegerField(blank=True,
-                                                             null=True)
-
-                choice = models.IntegerField(blank=True, null=True)
-                dice_output = models.IntegerField(blank=True, null=True)
-                error = models.TextField(blank=True, null=True)
-                fixation_time = models.IntegerField(blank=True, null=True)
-                gauge_level = models.IntegerField(blank=True, null=True)
-                inter_trial_time = models.IntegerField(blank=True, null=True)
-                left_beginning_angle = models.IntegerField(blank=True,
-                                                           null=True)
-                left_p = models.IntegerField(blank=True, null=True)
-                left_x0 = models.IntegerField(blank=True, null=True)
-                left_x1 = models.IntegerField(blank=True, null=True)
-                n_block = models.IntegerField(blank=True, null=True)
-                n_trial_inside_block = models.IntegerField(blank=True,
-                                                           null=True)
-                right_beginning_angle = models.IntegerField(blank=True,
-                                                            null=True)
-                right_p = models.IntegerField(blank=True, null=True)
-                right_x0 = models.IntegerField(blank=True, null=True)
-                right_x1 = models.IntegerField(blank=True, null=True)
-                time_to_come_back_to_the_grip = models.IntegerField(blank=True,
-                                                                    null=True)
-                time_to_decide = models.IntegerField(blank=True, null=True)
-
-                )
-
-
+                    date=datetime.strptime(trial.date, '%Y-%m-%d'),
+                    fixation_time_min=fixation_time[0],
+                    fixation_time_max=fixation_time[1],
+                    initial_stock=session.initial_stock,
+                    inter_trial_time_min=inter_trial_time[0],
+                    inter_trial_time_max=inter_trial_time[1],
+                    max_decision_time=sm.max_decision_time,
+                    max_return_time=sm.max_return_time,
+                    punishment_time=sm.punishment_time,
+                    result_display_time=sm.result_display_time,
+                    reward_time=sm.reward_time,
+                    valve_opening_time=sm.valve_opening_time,
+                    control_trials_proportion=sm.control_trial_proportion,
+                    incongruent_proportion=sm.incongruent_proportion,
+                    with_losses_proportion = sm.with_losses_proportion,
+                    choice=choice,
+                    dice_output=trial.dice_output,
+                    error=trial.error,
+                    fixation_time=trial.fixation_time,
+                    gauge_level=trial.gauge_level,
+                    inter_trial_time=trial.inter_trial_time,
+                    left_beginning_angle=trial.left_beginning_angle,
+                    left_p=int(trial.left_p),
+                    left_x0=int(trial.left_x0),
+                    right_beginning_angle=int(trial.right_beginning_angle),
+                    right_p=int(trial.right_p),
+                    right_x0=int(trial.right_x0),
+                    time_to_come_back_to_the_grip=
+                    trial.time_to_come_back_to_the_grip,
+                    time_to_decide=trial.time_to_decide)
 
 
 if __name__ == "__main__":
