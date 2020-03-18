e4c488fd545d029c3d1c99fbf1f334b85ae263c2
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-03-18 15:42:56 +0200

    update

--- import_xlsx.py
+++ import_xlsx.py
@@ -5,12 +5,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from experimental_data.import_export import import_data_xlsx, \
-    import_stimuli_xlsx
+from experimental_data.import_export import import_data_xlsx
 
 
 if __name__ == "__main__":
 
     # # For reproduction:
-    # import_xls('data_GH.xlsx')
+    # import_data_xlsx('data_GH.xlsx')
     import_data_xlsx()
